13e444c157853a12b6246a18f27c4712394cf724
Raghu Kaippully <rkaippully@gmail.com>
2022-08-29 10:11:55 +0530

    Upgrade to 1.0.4

--- README.md
+++ README.md
@@ -4,6 +4,18 @@ This is a basic CRUD app that operates on user resources.
 # Building and running
 
 ```shell
-stack build
-stack exec users
+nix run
+```
+
+or, if you'd like to get a development environment:
+
+```shell
+nix develop
+cabal run
+```
+
+Run postman tests:
+
+```shell
+postman
 ```
--- src/Main.hs
+++ src/Main.hs
@@ -1,62 +1,58 @@
-{-# LANGUAGE DataKinds                  #-}
-{-# LANGUAGE DeriveAnyClass             #-}
-{-# LANGUAGE DeriveGeneric              #-}
-{-# LANGUAGE DerivingVia                #-}
-{-# LANGUAGE DuplicateRecordFields      #-}
-{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE Arrows #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE OverloadedStrings          #-}
-{-# LANGUAGE QuasiQuotes                #-}
-{-# LANGUAGE RankNTypes                 #-}
-{-# LANGUAGE TypeApplications           #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
 module Main where
 
 import Control.Applicative (Alternative (..))
-import Control.Arrow (Kleisli (..))
+import Control.Category ((.))
+import Control.Monad (when)
 import Control.Monad.IO.Class (MonadIO (..))
 import Control.Monad.Reader (MonadReader (..), ReaderT (..))
-import Control.Monad.State.Strict (MonadState)
 import Data.Aeson (FromJSON, ToJSON)
-import Data.ByteString.Lazy (ByteString)
 import qualified Data.HashMap.Strict as HM
 import Data.Hashable (Hashable)
 import Data.IORef (IORef, modifyIORef, newIORef, readIORef)
 import Data.Maybe (isJust)
-import Data.Text (Text)
 import Data.Time.Calendar (Day)
 import GHC.Generics (Generic)
-import Network.HTTP.Types (StdMethod (..))
+import qualified Network.HTTP.Types as HTTP
 import Network.Wai (Application)
 import qualified Network.Wai.Handler.Warp as Warp
-import WebGear.Middlewares
-import WebGear.Trait
-import WebGear.Types
-
+import Web.HttpApiData (FromHttpApiData)
+import WebGear.Server
+import Prelude hiding ((.))
 
 --------------------------------------------------------------------------------
 -- An example program that uses WebGear to build a simple HTTP API to
 -- perform CRUD operations on user records.
 --------------------------------------------------------------------------------
 
-
 --------------------------------------------------------------------------------
 -- Model for users
 --------------------------------------------------------------------------------
 data User = User
-  { userId       :: UserId
-  , userName     :: Text
-  , dateOfBirth  :: Day
-  , gender       :: Gender
+  { userId :: UserId
+  , userName :: Text
+  , dateOfBirth :: Day
+  , gender :: Gender
   , emailAddress :: Text
   }
   deriving (Generic, FromJSON, ToJSON)
 
 newtype UserId = UserId Int
-  deriving (Eq, FromJSON, ToJSON, Hashable) via Int
+  deriving (Eq, FromJSON, ToJSON, Hashable, FromHttpApiData) via Int
 
 data Gender = Male | Female | OtherGender
   deriving (Generic, FromJSON, ToJSON)
-
 
 --------------------------------------------------------------------------------
 -- An in-memory store and associated operations for users
@@ -75,101 +71,154 @@ removeUser store@(UserStore ref) uid = liftIO $ do
   modifyIORef ref (HM.delete uid)
   pure $ isJust u
 
-
 --------------------------------------------------------------------------------
 -- Routes of the API
 --------------------------------------------------------------------------------
-type IntUserId = PathVar "userId" Int
+type IntUserId = PathVar "userId" UserId
 type Auth = BasicAuth App () Credentials
 
-authConfig :: BasicAuthConfig App () Credentials
-authConfig = BasicAuthConfig
-  { basicAuthRealm   = "Wakanda"
-  , toBasicAttribute = \creds -> pure $
-                         if creds == Credentials "panther" "forever"
-                         then Right creds
-                         else Left ()
-  }
+authConfig :: BasicAuth App () Credentials
+authConfig =
+  BasicAuth'
+    { toBasicAttribute = \creds ->
+        pure $
+          if creds == Credentials "panther" "forever"
+            then Right creds
+            else Left ()
+    }
 
 -- The route handlers run in the App monad
-newtype App a = App { unApp :: ReaderT UserStore Router a }
-  deriving newtype ( Functor
-                   , Applicative
-                   , Alternative
-                   , Monad
-                   , MonadIO
-                   , MonadReader UserStore
-                   , MonadState PathInfo
-                   , MonadRouter
-                   )
+newtype App a = App {unApp :: ReaderT UserStore IO a}
+  deriving newtype
+    ( Functor
+    , Applicative
+    , Alternative
+    , Monad
+    , MonadIO
+    , MonadReader UserStore
+    )
 
-userRoutes :: Handler App '[] ByteString
-userRoutes = [match| /v1/users/userId:Int |]   -- non-TH version: path @"/v1/users" . pathVar @"userId" @Int
-               (publicRoutes <|> protectedRoutes)
+userRoutes :: RequestHandler (ServerHandler App) req
+userRoutes =
+  [match| /v1/users/userId:UserId |] -- non-TH version: path @"/v1/users" . pathVar @"userId" @Int
+    (publicRoutes <+> protectedRoutes)
 
 -- | Routes accessible without any authentication
-publicRoutes :: HasTrait IntUserId req => Handler App req ByteString
+publicRoutes ::
+  ( HasTrait IntUserId req
+  , StdHandler
+      h
+      App
+      '[]
+      '[RequiredHeader "Content-Type" Text, JSONBody User]
+  ) =>
+  RequestHandler h req
 publicRoutes = getUser
 
 -- | Routes that require HTTP basic authentication
-protectedRoutes :: HasTrait IntUserId req => Handler App req ByteString
-protectedRoutes = basicAuth authConfig
-                  $ putUser <|> deleteUser
-
-getUser :: HasTrait IntUserId req => Handler App req ByteString
-getUser = method @GET
-          $ jsonResponseBody @User
-          $ handler
+protectedRoutes ::
+  forall h req.
+  ( HasTrait IntUserId req
+  , StdHandler
+      h
+      App
+      '[BasicAuth App () Credentials, JSONBody User]
+      '[RequiredHeader "Content-Type" Text, JSONBody User, Body Text]
+  ) =>
+  RequestHandler h req
+protectedRoutes = basicAuth authConfig forbidden $ putUser <+> deleteUser
   where
-    handler :: HasTrait IntUserId req => Handler App req User
-    handler = Kleisli $ \request -> do
+    forbidden :: h a Response
+    forbidden = proc _ -> unlinkA . respondA @Text HTTP.forbidden403 "text/plain" -< "Unauthorized"
+
+getUser ::
+  forall h req.
+  ( HasTrait IntUserId req
+  , StdHandler
+      h
+      App
+      '[]
+      '[RequiredHeader "Content-Type" Text, JSONBody User]
+  ) =>
+  RequestHandler h req
+getUser = method HTTP.GET $
+  proc request -> do
+    let uid = pick @IntUserId $ from request
+    maybeUser <- fetchUser -< uid
+    case maybeUser of
+      Nothing -> unlinkA . notFound404 -< ()
+      Just user -> unlinkA . respondJsonA @User HTTP.ok200 -< user
+  where
+    fetchUser :: h UserId (Maybe User)
+    fetchUser = arrM $ \uid -> do
+      store <- ask
+      lookupUser store uid
+
+putUser ::
+  forall h req.
+  ( HaveTraits [Auth, IntUserId] req
+  , StdHandler
+      h
+      App
+      '[JSONBody User]
+      '[RequiredHeader "Content-Type" Text, JSONBody User, Body Text]
+  ) =>
+  RequestHandler h req
+putUser = method HTTP.PUT $
+  jsonRequestBody @User badRequestBody $
+    proc request -> do
       let uid = pick @IntUserId $ from request
-      store <- ask
-      user <- lookupUser store (UserId uid)
-      pure $ maybe notFound404 ok200 user
-
-putUser :: HaveTraits [Auth, IntUserId] req => Handler App req ByteString
-putUser = method @PUT
-          $ requestContentTypeHeader @"application/json"
-          $ jsonRequestBody @User
-          $ jsonResponseBody @User
-          $ handler
+          user = pick @(JSONBody User) $ from request
+          user' = user{userId = uid}
+      doAdd -< (request, user')
+      unlinkA . respondJsonA @User HTTP.ok200 -< user'
   where
-    handler :: HaveTraits [Auth, IntUserId, JSONBody User] req => Handler App req User
-    handler = Kleisli $ \request -> do
-      let uid   = pick @IntUserId $ from request
-          user  = pick @(JSONBody User) $ from request
-          user' = user { userId = UserId uid }
+    badRequestBody :: h a Response
+    badRequestBody = proc _ ->
+      unlinkA . respondA @Text HTTP.badRequest400 "text/plain" -< "Could not parse body"
+
+    doAdd = arrM $ \(request, user) -> do
       store <- ask
-      addUser store user'
+      addUser store user
       logActivity request "updated"
-      pure $ ok200 user'
 
-deleteUser :: HaveTraits [Auth, IntUserId] req => Handler App req ByteString
-deleteUser = method @DELETE handler
+deleteUser ::
+  forall h req.
+  ( HaveTraits [Auth, IntUserId] req
+  , StdHandler
+      h
+      App
+      '[]
+      '[]
+  ) =>
+  RequestHandler h req
+deleteUser = method HTTP.DELETE $
+  proc request -> do
+    let uid = pick @IntUserId $ from request
+    removed <- doRemove -< (request, uid)
+    if removed
+      then unlinkA . noContent204 -< ()
+      else unlinkA . notFound404 -< ()
   where
-    handler :: HaveTraits [Auth, IntUserId] req => Handler App req ByteString
-    handler = Kleisli $ \request -> do
-      let uid = pick @IntUserId $ from request
+    doRemove = arrM $ \(request, uid) -> do
       store <- ask
-      found <- removeUser store (UserId uid)
-      if found
-        then logActivity request "deleted" >> pure noContent204
-        else pure notFound404
+      found <- removeUser store uid
+      when found $
+        logActivity request "deleted"
+      pure found
 
 logActivity :: (MonadIO m, HasTrait Auth req) => Linked req Request -> String -> m ()
 logActivity request msg = do
   let name = credentialsUsername $ pick @Auth $ from request
   liftIO $ putStrLn $ msg <> ": by " <> show name
 
-
 --------------------------------------------------------------------------------
--- | The application server
---------------------------------------------------------------------------------
+-- The application server
+-------------------------------------------------------------------------------
 application :: UserStore -> Application
 application store = toApplication $ transform appToRouter userRoutes
   where
-    appToRouter :: App a -> Router a
+    appToRouter :: App a -> IO a
     appToRouter = flip runReaderT store . unApp
 
 main :: IO ()
